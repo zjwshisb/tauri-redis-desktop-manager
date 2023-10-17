c8ae4508d0a0beeff1244f61696fbac3f1db6814
周建威 <361340272@qq.com>
2023-10-17 11:23:41 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -12,29 +12,32 @@ export default defineConfig(async () => ({
   // tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
-    strictPort: true,
+    strictPort: true
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      '@': path.resolve(__dirname, './src')
     }
   },
   // to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  envPrefix: ['VITE_', 'TAURI_'],
   build: {
     // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        debug: path.resolve(__dirname, "src/windows/debug/index.html"),
-        detail: path.resolve(__dirname, "src/windows/detail/index.html")
+        database: path.resolve(__dirname, 'src/windows/database/index.html'),
+        detail: path.resolve(__dirname, 'src/windows/detail/index.html'),
+        log: path.resolve(__dirname, 'src/windows/log/index.html'),
+        migrate: path.resolve(__dirname, 'src/windows/migrate/index.html'),
+        session: path.resolve(__dirname, 'src/windows/session/index.html')
       }
     }
-  },
-}));
+  }
+}))
