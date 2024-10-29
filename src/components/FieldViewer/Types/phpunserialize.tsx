9fcdf4388280e5a6df0eda3b6b005784b75a9a65
zhoujianwei <zhoujianwei@sailvan.com>
2024-10-29 14:50:18 +0800

    update version

--- src/components/FieldViewer/Types/phpunserialize.tsx
+++ src/components/FieldViewer/Types/phpunserialize.tsx
@@ -1,26 +1,26 @@
-import { type TypeFormat } from '.'
-import React from 'react'
-import Error from '../Err'
-import request from '@/utils/request'
-
-const item: TypeFormat = {
-  key: 'php_unserialize',
-  label: 'Php Unserialize',
-  render: async (content) => {
-    try {
-      const res = await request<string>(
-        'transfer/php_unserialize',
-        0,
-        {
-          data: content
-        },
-        { showNotice: false }
-      )
-      return res.data
-    } catch (err) {
-      return <Error message={err as string} />
-    }
-  }
-}
-
-export default item
+// import { type TypeFormat } from '.'
+// import React from 'react'
+// import Error from '../Err'
+// import request from '@/utils/request'
+//
+// const item: TypeFormat = {
+//   key: 'php_unserialize',
+//   label: 'Php Unserialize',
+//   render: async (content) => {
+//     try {
+//       const res = await request<string>(
+//         'transfer/php_unserialize',
+//         0,
+//         {
+//           data: content
+//         },
+//         { showNotice: false }
+//       )
+//       return res.data
+//     } catch (err) {
+//       return <Error message={err as string} />
+//     }
+//   }
+// }
+//
+// export default item
